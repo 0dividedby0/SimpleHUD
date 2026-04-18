4628230d9c5ae485c6209fb06a63d59624688d2a
0dividedby0 <jason.halcomb@wsu.edu>
2026-04-18 14:16:33 -0700

    Removing unused import

--- src/main/java/com/dividedby0/simplehud/client/ClientHudEvents.java
+++ src/main/java/com/dividedby0/simplehud/client/ClientHudEvents.java
@@ -15,7 +15,6 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.level.LightLayer;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.HitResult;
